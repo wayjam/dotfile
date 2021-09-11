d712deb906dbf29028c6bfdc5b99d4e70bfc7960
WayJam So <imsuwj@gmail.com>
2021-09-11 21:00:42 +0800

    update hammerspoon

--- .hammerspoon/spoons.lua
+++ .hammerspoon/spoons.lua
@@ -1,122 +1,133 @@
-hspoon_list = {
-	"AClock",
-	"FnMate",
-  "VimMode",
-  "WinWin"
-}
+hspoon_list = {"AClock", "FnMate", "VimMode", "WinWin"}
 
 -- Load those Spoons
-for _, v in pairs(hspoon_list) do
-    hs.loadSpoon(v)
+for _, v in pairs(hspoon_list) do hs.loadSpoon(v) end
+
+function activateAppM()
+    spoon.ModalMgr:deactivateAll()
+    -- Show the keybindings cheatsheet once appM is activated
+    spoon.ModalMgr:activate({"appM"}, "#5c6370", true)
 end
 
-hswhints_keys = {localHyperKey, "tab"}
-if string.len(hswhints_keys[2]) > 0 then
-    spoon.ModalMgr.supervisor:bind(hswhints_keys[1], hswhints_keys[2], 'Show Window Hints', function()
-        spoon.ModalMgr:deactivateAll()
-        hs.hints.windowHints()
-    end)
+function activateWindowHints()
+    spoon.ModalMgr:deactivateAll()
+    hs.hints.windowHints()
 end
+
+function activateLockScreen() hs.caffeinate.lockScreen() end
 
 spoon.ModalMgr:new("appM")
-local cmodal = spoon.ModalMgr.modal_list["appM"]
-cmodal:bind('', 'escape', 'Deactivate appM', function() spoon.ModalMgr:deactivate({"appM"}) end)
-cmodal:bind('', 'Q', 'Deactivate appM', function() spoon.ModalMgr:deactivate({"appM"}) end)
-cmodal:bind('', 'tab', 'Toggle Cheatsheet', function() spoon.ModalMgr:toggleCheatsheet() end)
-if not hsapp_list then
-    hsapp_list = {
-        {key = 'f', name = 'Finder'},
-        {key = 'g', name = 'Google Chrome'},
-        {key = 't', name = 'Terminal'},
-        {key = 'v', id = 'com.apple.ActivityMonitor'},
-        {key = 'y', id = 'com.apple.systempreferences'},
-    }
-end
+appModal = spoon.ModalMgr.modal_list["appM"]
+appModal:bind('', 'escape', 'Deactivate appM', function() spoon.ModalMgr:deactivate({"appM"}) end)
+appModal:bind('', 'Q', 'Deactivate appM', function() spoon.ModalMgr:deactivate({"appM"}) end)
+appModal:bind('', 'tab', 'Toggle Cheatsheet', function() spoon.ModalMgr:toggleCheatsheet() end)
+hsapp_list = {
+    {key = 'f', name = 'Finder'},
+    {key = 'g', name = 'Google Chrome'},
+    {key = 't', name = 'Terminal'},
+    {key = 'v', id = 'com.apple.ActivityMonitor'},
+    {key = 'y', id = 'com.apple.systempreferences'}
+}
 for _, v in ipairs(hsapp_list) do
     if v.id then
         local located_name = hs.application.nameForBundleID(v.id)
         if located_name then
-            cmodal:bind('', v.key, located_name, function()
-                hs.application.launchOrFocusByBundleID(v.id)
-                spoon.ModalMgr:deactivate({"appM"})
-            end)
+            appModal:bind(
+                '', v.key, located_name, function()
+                    hs.application.launchOrFocusByBundleID(v.id)
+                    spoon.ModalMgr:deactivate({"appM"})
+                end)
         end
     elseif v.name then
-        cmodal:bind('', v.key, v.name, function()
-            hs.application.launchOrFocus(v.name)
-            spoon.ModalMgr:deactivate({"appM"})
-        end)
+        appModal:bind(
+            '', v.key, v.name, function()
+                hs.application.launchOrFocus(v.name)
+                spoon.ModalMgr:deactivate({"appM"})
+            end)
     end
 end
 
--- Then we register some keybindings with modal supervisor
-hsappM_keys = hsappM_keys or { hyperKey, "A" }
-if string.len(hsappM_keys[2]) > 0 then
-    spoon.ModalMgr.supervisor:bind(hsappM_keys[1], hsappM_keys[2], "Enter AppM Environment", function()
-        spoon.ModalMgr:deactivateAll()
-        -- Show the keybindings cheatsheet once appM is activated
-        spoon.ModalMgr:activate({"appM"}, "#FFBD2E", true)
-    end)
-end
+spoon.ModalMgr.supervisor:bind(localHyperKey, "Tab", 'Show Window Hints', activateWindowHints)
+spoon.ModalMgr.supervisor:bind(hyperKey, "L", "Lock Screen", activateLockScreen)
+spoon.ModalMgr.supervisor:bind(hyperKey, "A", "Enter AppM Environment", activateAppM)
 
 --- Register VimMode
 if spoon.VimMode then
-  local vim = spoon.VimMode:new()
+    local vim = spoon.VimMode:new()
 
-  -- Configure apps you do *not* want Vim mode enabled in
-  -- For example, you don't want this plugin overriding your control of Terminal
-  -- vim
-  vim
-    :disableForApp('Code')
-    :disableForApp('zoom.us')
-    :disableForApp('iTerm')
-    :disableForApp('iTerm2')
-    :disableForApp('Terminal')
-    :disableForApp('Alacritty')
+    -- Configure apps you do *not* want Vim mode enabled in
+    -- For example, you don't want this plugin overriding your control of Terminal
+    -- vim
+    vim:disableForApp('Code'):disableForApp('zoom.us'):disableForApp('iTerm'):disableForApp('iTerm2'):disableForApp(
+        'Terminal'):disableForApp('Alacritty')
 
-  -- If you want the screen to dim (a la Flux) when you enter normal mode
-  -- flip this to true.
-  vim:shouldDimScreenInNormalMode(false)
+    -- If you want the screen to dim (a la Flux) when you enter normal mode
+    -- flip this to true.
+    vim:shouldDimScreenInNormalMode(false)
 
-  -- If you want to show an on-screen alert when you enter normal mode, set
-  -- this to true
-  vim:shouldShowAlertInNormalMode(true)
+    -- If you want to show an on-screen alert when you enter normal mode, set
+    -- this to true
+    vim:shouldShowAlertInNormalMode(true)
 
-  -- You can configure your on-screen alert font
-  vim:setAlertFont("Courier New")
+    -- You can configure your on-screen alert font
+    vim:setAlertFont("Courier New")
 
-  -- Enter normal mode by typing a key sequence
-  -- vim:enterWithSequence('jk')
+    -- Enter normal mode by typing a key sequence
+    -- vim:enterWithSequence('jk')
 
-  -- if you want to bind a single key to entering vim, remove the
-  -- :enterWithSequence('jk') line above and uncomment the bindHotKeys line
-  -- below:
-  --
-  -- To customize the hot key you want, see the mods and key parameters at:
-  --   https://www.hammerspoon.org/docs/hs.hotkey.html#bind
-  --
-  vim:bindHotKeys({ enter = { hyperKey, ';' } })
+    -- if you want to bind a single key to entering vim, remove the
+    -- :enterWithSequence('jk') line above and uncomment the bindHotKeys line
+    -- below:
+    --
+    -- To customize the hot key you want, see the mods and key parameters at:
+    --   https://www.hammerspoon.org/docs/hs.hotkey.html#bind
+    --
+    vim:bindHotKeys({enter = {hyperKey, ';'}})
 end
 
 --- Register AClock
 if spoon.AClock then
-    hsaclock_keys = { hyperKey, "t" }
-    if string.len(hsaclock_keys[2]) > 0 then
-        spoon.ModalMgr.supervisor:bind(hsaclock_keys[1], hsaclock_keys[2], "Toggle Floating Clock", function() spoon.AClock:toggleShow() end)
-    end
+    spoon.ModalMgr.supervisor:bind(hyperKey, "T", "Toggle Floating Clock", function() spoon.AClock:toggleShow() end)
 end
 
 --- Register WinWin
 if spoon.WinWin then
     spoon.ModalMgr:new("resizeM")
     local cmodal = spoon.ModalMgr.modal_list["resizeM"]
+
+    local function activateResizeM()
+        -- Deactivate some modal environments or not before activating a new one
+        spoon.ModalMgr:deactivateAll()
+        -- Show an status indicator so we know we're in some modal environment now
+        spoon.ModalMgr:activate({"resizeM"}, "#B22222")
+    end
+
+    local toggleMaximizedMap = {}
+
+    function maximizeWin()
+        local win = hs.window.frontmostWindow()
+        if not (win:isMaximizable()) then return end
+        win:application():activate(true)
+        win:application():unhide()
+        win:focus()
+        win:maximize(0)
+    end
+
     cmodal:bind('', 'escape', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
     cmodal:bind('', 'Q', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
     cmodal:bind('', 'tab', 'Toggle Cheatsheet', function() spoon.ModalMgr:toggleCheatsheet() end)
-    cmodal:bind('', 'A', 'Move Leftward', function() spoon.WinWin:stepMove("left") end, nil, function() spoon.WinWin:stepMove("left") end)
-    cmodal:bind('', 'D', 'Move Rightward', function() spoon.WinWin:stepMove("right") end, nil, function() spoon.WinWin:stepMove("right") end)
-    cmodal:bind('', 'W', 'Move Upward', function() spoon.WinWin:stepMove("up") end, nil, function() spoon.WinWin:stepMove("up") end)
-    cmodal:bind('', 'S', 'Move Downward', function() spoon.WinWin:stepMove("down") end, nil, function() spoon.WinWin:stepMove("down") end)
+    cmodal:bind(
+        '', 'A', 'Move Leftward', function() spoon.WinWin:stepMove("left") end, nil,
+        function() spoon.WinWin:stepMove("left") end)
+    cmodal:bind(
+        '', 'D', 'Move Rightward', function() spoon.WinWin:stepMove("right") end, nil,
+        function() spoon.WinWin:stepMove("right") end)
+    cmodal:bind(
+        '', 'W', 'Move Upward', function() spoon.WinWin:stepMove("up") end, nil,
+        function() spoon.WinWin:stepMove("up") end)
+    cmodal:bind(
+        '', 'S', 'Move Downward', function() spoon.WinWin:stepMove("down") end, nil,
+        function() spoon.WinWin:stepMove("down") end)
     cmodal:bind('', 'H', 'Lefthalf of Screen', function() spoon.WinWin:moveAndResize("halfleft") end)
     cmodal:bind('', 'L', 'Righthalf of Screen', function() spoon.WinWin:moveAndResize("halfright") end)
     cmodal:bind('', 'K', 'Uphalf of Screen', function() spoon.WinWin:moveAndResize("halfup") end)
@@ -127,12 +138,25 @@ if spoon.WinWin then
     cmodal:bind('', 'I', 'SouthEast Corner', function() spoon.WinWin:moveAndResize("cornerSE") end)
     cmodal:bind('', 'F', 'Fullscreen', function() spoon.WinWin:moveAndResize("fullscreen") end)
     cmodal:bind('', 'C', 'Center Window', function() spoon.WinWin:moveAndResize("center") end)
-    cmodal:bind('', '=', 'Stretch Outward', function() spoon.WinWin:moveAndResize("expand") end, nil, function() spoon.WinWin:moveAndResize("expand") end)
-    cmodal:bind('', '-', 'Shrink Inward', function() spoon.WinWin:moveAndResize("shrink") end, nil, function() spoon.WinWin:moveAndResize("shrink") end)
-    cmodal:bind('shift', 'H', 'Move Leftward', function() spoon.WinWin:stepResize("left") end, nil, function() spoon.WinWin:stepResize("left") end)
-    cmodal:bind('shift', 'L', 'Move Rightward', function() spoon.WinWin:stepResize("right") end, nil, function() spoon.WinWin:stepResize("right") end)
-    cmodal:bind('shift', 'K', 'Move Upward', function() spoon.WinWin:stepResize("up") end, nil, function() spoon.WinWin:stepResize("up") end)
-    cmodal:bind('shift', 'J', 'Move Downward', function() spoon.WinWin:stepResize("down") end, nil, function() spoon.WinWin:stepResize("down") end)
+    cmodal:bind('', 'M', 'Maximize Window', maximizeWin, maximizeWin, maximizeWin)
+    cmodal:bind(
+        '', '=', 'Stretch Outward', function() spoon.WinWin:moveAndResize("expand") end, nil,
+        function() spoon.WinWin:moveAndResize("expand") end)
+    cmodal:bind(
+        '', '-', 'Shrink Inward', function() spoon.WinWin:moveAndResize("shrink") end, nil,
+        function() spoon.WinWin:moveAndResize("shrink") end)
+    cmodal:bind(
+        'shift', 'H', 'Move Leftward', function() spoon.WinWin:stepResize("left") end, nil,
+        function() spoon.WinWin:stepResize("left") end)
+    cmodal:bind(
+        'shift', 'L', 'Move Rightward', function() spoon.WinWin:stepResize("right") end, nil,
+        function() spoon.WinWin:stepResize("right") end)
+    cmodal:bind(
+        'shift', 'K', 'Move Upward', function() spoon.WinWin:stepResize("up") end, nil,
+        function() spoon.WinWin:stepResize("up") end)
+    cmodal:bind(
+        'shift', 'J', 'Move Downward', function() spoon.WinWin:stepResize("down") end, nil,
+        function() spoon.WinWin:stepResize("down") end)
     cmodal:bind('', 'left', 'Move to Left Monitor', function() spoon.WinWin:moveToScreen("left") end)
     cmodal:bind('', 'right', 'Move to Right Monitor', function() spoon.WinWin:moveToScreen("right") end)
     cmodal:bind('', 'up', 'Move to Above Monitor', function() spoon.WinWin:moveToScreen("up") end)
@@ -143,21 +167,6 @@ if spoon.WinWin then
     cmodal:bind('', '`', 'Center Cursor', function() spoon.WinWin:centerCursor() end)
 
     -- Register resizeM with modal supervisor
-    hsresizeM_keys = { hyperKey, "w" }
-    if string.len(hsresizeM_keys[2]) > 0 then
-        spoon.ModalMgr.supervisor:bind(hsresizeM_keys[1], hsresizeM_keys[2], "Enter resizeM Environment", function()
-            -- Deactivate some modal environments or not before activating a new one
-            spoon.ModalMgr:deactivateAll()
-            -- Show an status indicator so we know we're in some modal environment now
-            spoon.ModalMgr:activate({"resizeM"}, "#B22222")
-        end)
-    end
-end
-
--- Register lock screen
-hslock_keys = { hyperKey, "l" }
-if string.len(hslock_keys[2]) > 0 then
-    spoon.ModalMgr.supervisor:bind(hslock_keys[1], hslock_keys[2], "Lock Screen", function()
-        hs.caffeinate.lockScreen()
-    end)
+    spoon.ModalMgr.supervisor:bind(hyperKey, "Z", "Enter resizeM Environment", activateResizeM)
+    appModal:bind('', 'R', 'Resize', activateResizeM)
 end
